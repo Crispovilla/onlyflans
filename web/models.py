023c555454e9d047b1a8fa235d7278953e9b754f
cristobal <cristobal.villarroelalvarado@gmail.com>
2024-08-05 19:36:09 -0400

    hito 3

--- web/models.py
+++ web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Flan(models.Model):
+    flan_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField(default='https://acortar.link/Hs3SLR')
+    slug = models.SlugField(max_length=64, unique=True, default='default-slug')
+    is_private = models.BooleanField()
+
+
+class ContactForm(models.Model):
+    contact_form_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    customer_email = models.EmailField()
+    customer_name = models.CharField(max_length=64)
+    message = models.TextField()
--- web/views.py
+++ web/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ContactFormForm
+from .models import Flan, ContactForm
 # Create your views here.
 def indice(request):
-    return render(request, 'index.html', {} )
+    flanes = Flan.objects.all()
+    
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    
+    return render(request, 'index.html', {'flanes_publicos': flanes_publicos} )
 
 def acerca(request):
     return render(request, 'about.html', {} )
 
 def bienvenido(request):
-    return render(request, 'wellcome.html', {} )
+    flanes_privados = Flan.objects.filter(is_private = True)
+    return render(request, 'wellcome.html', {'flanes_privados': flanes_privados} )
 
+def contacto(request):
+    if request.method =='POST':
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+            return HttpResponseRedirect('/exito')
+    else:
+        form = ContactFormForm()
+    return render(request, 'contacto.html', {'form': form})
+
+def exito(request):
+    return render(request, 'success.html', {})
